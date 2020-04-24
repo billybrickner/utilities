d47abd2e03f075b411b64dbdc1ec19fb8099d310
Billy Brickner <billybrickner@gmail.com>
2020-04-24 18:18:16 -0400

    Update tag

--- scripts/addTags.sh
+++ scripts/addTags.sh
@@ -1,1 +1,3 @@
-ctags -R .
+TAG_FILE=tags
+ctags -R -a -o $TAG_FILE --exclude=html --exclude=*.css --exclude=*.js .
+ctags -R -a -o $TAG_FILE --languages=python --python-kinds=-iv --exclude=html --exclude=*.css --exclude=*.js .
