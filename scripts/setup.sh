688c7e42639b8d0cccb171fe47ee9b8f0bd4ae96
Billy Brickner <billybrickner@gmail.com>
2021-08-15 17:17:25 -0400

    Update script to be more accurate

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,9 +3,13 @@ PATH_TO_PROFILES=~/utilities/profiles
 pushd ~
 echo ". $PATH_TO_PROFILES/.my_bash_profile.sh" >> .bashrc
 mkdir ".vim"
-pushd .vim && mkdir spell && popd
-mkdir cache && pushd .cache && mkdir vim && popd
+pushd ".vim" && mkdir spell && popd
+mkdir ".cache" && pushd ".cache" && mkdir vim && popd
 cp $PATH_TO_PROFILES/.vimrc .
-source .bashrc
 popd
-gitSetup.sh
+echo "Now run:"
+echo "pushd ~"
+echo "source .bashrc"
+echo "pushd utilities"
+echo "gitSetup.sh"
+echo "popd && popd"
