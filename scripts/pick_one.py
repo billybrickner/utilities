b1f879828a2b7e5ce51b2aaf91f2de69c1d6dcfa
Billy Brickner <billybrickner@gmail.com>
2021-10-09 21:41:01 -0400

    Update loser messages to file

--- scripts/pick_one.py
+++ scripts/pick_one.py
@@ -1,7 +1,13 @@
 #! /usr/bin/env python3
 
-import time, random, argparse
+import time, random, argparse, os
 
+
+
+def getMessages(filename):
+   with open(filename, 'r') as f:
+      messages = [line.strip() for line in f]
+   return messages
 
 def getNextChoice(choices):
    return input("\toption %2d:" % (len(choices) + 1))
@@ -10,48 +16,8 @@ def prettyPrint(choices):
    print("Choices: " + ", ".join(choices))
 
 def roll(choices):
-   loserMessages = ["went out for a pack of cigarettes",
-                     "took a long walk off a short pier",
-                     "is now using the pronouns was/were",
-                     "is just resting",
-                     "did their own research and found a cure for COVID",
-                     "is pinin' for the Fjords",
-                     "is living on a farm upstate",
-                     "... press F in the chat",
-                     "claimed their final reward",
-                     "didn't wake up after September ended",
-                     "has gone west",
-                     "is now apart of a graveyard smash",
-                     "exited stage down",
-                     "is now at room temperature",
-                     "became a root inspector",
-                     "has invested in a pine condo",
-                     "is definitely done dancing",
-                     "has gone home feet first",
-                     "has just invited their friends and family to a necktie social",
-                     "is calling collect from the horizontal phone booth",
-                     "just got stamped \"return to sender\"",
-                     "kicked the oxygen habit",
-                     "is picking turnips with a stepladder",
-                     "was terminated",
-                     "was eliminated",
-                     "died to doomblade",
-                     "was murdered",
-                     "was the victim of a heartless act",
-                     "incurred a fatal push",
-                     "is not a dead ringer",
-                     "was WASTED",
-                     "just got done starring in a slasher film",
-                     "has a date with Charon",
-                     "is now SANS UNDERTALE",
-                     "was on the ground when the bodies hit the floor",
-                     "got a game over",
-                     "reenacted the history the confederacy very quickly",
-                     "is getting picked up by Mori Calliope later",
-                     "reenacted president Lincoln's final play",
-                     "reenacted JFK's drive through downtown",
-                     "has their own personal greek choir",
-                     "pulled the plug"]
+   path_to_loser_messages = str(os.environ["HOME"]) + "/utilities/profiles/loserMessages.txt"
+   loserMessages = getMessages(path_to_loser_messages)
 
    print("rolling",end="", flush=True)
    for i in range(3):
@@ -87,7 +53,7 @@ def getCmdLineChoices():
    print(args.choices)
    return args.choices
 
-def main():
+def pickOne():
    choices = getCmdLineChoices()
 
    if len(choices) < 2:
@@ -101,6 +67,9 @@ def main():
          prettyPrint(choices)
 
    print("Winner:", choices[0])
+
+def main():
+   pickOne()
 
 if __name__ == '__main__':
    main()
