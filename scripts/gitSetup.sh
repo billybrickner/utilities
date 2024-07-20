c123fc2361a8413a3dfd570611a28282daf1958e
Billy Brickner <billybrickner@gmail.com>
2024-07-20 10:50:16 -0700

    update setup

--- scripts/gitSetup.sh
+++ scripts/gitSetup.sh
@@ -2,3 +2,5 @@
 git config --global user.email "billybrickner@gmail.com"
 git config --global user.name "Billy Brickner"
 git config credential.helper 'cache --timeout=30000'
+git config --global core.editor "vim"
+git config pull.rebase false
